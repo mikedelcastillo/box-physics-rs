c1d5240a69a7f7233309bfa8fb6ab8ddfe8f4093
mikedelcastillo <johnmichaeldc@gmail.com>
2023-10-24 23:58:03 +0800

    feat: use get_many, computation for constraint

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_inspector_egui::{prelude::ReflectInspectorOptions, InspectorOptions};
 
 pub const PHYSICS_DT: f32 = 1.0 / 20.0;
+pub const PHYSICS_ITERS: u8 = 1;
 
 fn main() {
     App::new()
@@ -19,7 +20,7 @@ fn main() {
         .register_type::<Point>()
         .register_type::<Constraint>()
         .add_systems(Startup, (spawn_entities).chain())
-        .add_systems(Update, (apply_velocities).chain()
+        .add_systems(Update, (compute_constraints, update_positions).chain()
             .run_if(on_timer(Duration::from_millis((1000.0 * PHYSICS_DT) as u64))))
         .add_systems(Update, (debug_points, debug_constraints))
         .run();
@@ -29,7 +30,6 @@ fn main() {
 #[reflect(InspectorOptions)]
 pub struct Point {
     pub position: Vec2,
-    pub velocity: Vec2,
     pub prev_position: Vec2,
     pub radius: f32,
     pub mass: f32,
@@ -47,7 +47,6 @@ pub struct Constraint {
 pub fn make_point(position: Vec2) -> impl Bundle {
     Point {
         position,
-        velocity: Vec2::ZERO,
         prev_position: position,
         radius: 1.0,
         mass: 10.0,
@@ -73,12 +72,50 @@ pub fn spawn_entities(mut commands: Commands) {
     commands.spawn(make_constraint(b, c, 100.0));
     commands.spawn(make_constraint(c, d, 100.0));
     commands.spawn(make_constraint(d, a, 100.0));
+    commands.spawn(make_constraint(a, c, 100.0 * 2.0_f32.sqrt()));
+    commands.spawn(make_constraint(b, d, 100.0 * 2.0_f32.sqrt()));
 }
 
-pub fn apply_velocities(mut point_query: Query<&mut Point>) {
+macro_rules! constraint_points {
+    ($constraint:ident, $point_query:ident, $point_a:ident, $point_b:ident, $then:block) => {
+        let result = $point_query.get_many([$constraint.point_a, $constraint.point_b]);
+        if let Ok([$point_a, $point_b]) = result $then
+    };
+}
+
+macro_rules! constraint_points_mut {
+    ($constraint:ident, $point_query:ident, $point_a:ident, $point_b:ident, $then:block) => {
+        let result = $point_query.get_many_mut([$constraint.point_a, $constraint.point_b]);
+        if let Ok([mut $point_a, mut $point_b]) = result $then
+    };
+}
+
+pub fn compute_constraints(
+    mut point_query: Query<&mut Point>,
+    constraint_query: Query<&Constraint>,
+) {
+    for _ in 0..PHYSICS_ITERS {
+        for constraint in constraint_query.iter() {
+            constraint_points_mut!(constraint, point_query, point_a, point_b, {
+                let delta = point_b.position - point_a.position;
+                let distance = point_a.position.distance(point_b.position);
+                let diff = (constraint.length - distance) / distance * constraint.strength;
+                let offset = delta * diff * 0.5;
+                let effect_a = (1.0 / point_a.mass) / ((1.0 / point_a.mass) + (1.0 / point_b.mass));
+                let effect_b = 1.0 - effect_a;
+    
+                point_a.position -= offset * effect_a;
+                point_b.position += offset * effect_b;
+            });
+        }
+    }
+}
+
+pub fn update_positions(mut point_query: Query<&mut Point>) {
     for mut point in point_query.iter_mut() {
+        let velocity= point.position - point.prev_position;
         point.prev_position = point.position;
-        point.position = point.position + point.velocity;
+        point.position += velocity;
     }
 }
 
@@ -86,16 +123,6 @@ pub fn debug_points(mut gizmos: Gizmos, point_query: Query<&Point>) {
     for point in point_query.iter() {
         gizmos.circle_2d(point.position, point.radius, Color::WHITE);
     }
-}
-
-macro_rules! constraint_points {
-    ($constraint:ident, $point_query:ident, $point_a:ident, $point_b:ident, $then:block) => {
-        let point_a = $point_query.get($constraint.point_a);
-        if let Ok($point_a) = point_a {
-            let point_b = $point_query.get($constraint.point_b);
-            if let Ok($point_b) = point_b $then
-        }
-    };
 }
 
 pub fn debug_constraints(
