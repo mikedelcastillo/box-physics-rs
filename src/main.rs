0922c2eef7e0ef6160a03a40965a9151e6fbb444
mikedelcastillo <johnmichaeldc@gmail.com>
2023-10-24 23:34:03 +0800

    chore: use gizmo, switch to velret integration

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::f32::consts::PI;
 use std::time::Duration;
 
+use bevy::prelude::Entity;
 use bevy::time::common_conditions::on_timer;
 use bevy::{log::LogPlugin, prelude::*};
-use bevy_prototype_lyon::prelude::{Fill, GeometryBuilder, ShapeBundle, ShapePlugin};
-use bevy_prototype_lyon::shapes::{Circle, Rectangle, RectangleOrigin};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_inspector_egui::{prelude::ReflectInspectorOptions, InspectorOptions};
 
 pub const PHYSICS_DT: f32 = 1.0 / 20.0;
 
@@ -15,163 +15,97 @@ fn main() {
             filter: "info,wgpu_core=off,wgpu_hal=off,bevy_input=off,bevy_render=off,bevy_diagnostic=off".into(),
             level: bevy::log::Level::DEBUG,
         }))
-        .add_plugins(ShapePlugin)
-        .add_systems(Startup, (spawn_camera, spawn_bodies).chain())
-        .add_systems(Update, (apply_velocities, update_transforms).chain()
+        .add_plugins(WorldInspectorPlugin::new())
+        .register_type::<Point>()
+        .register_type::<Constraint>()
+        .add_systems(Startup, (spawn_entities).chain())
+        .add_systems(Update, (apply_velocities).chain()
             .run_if(on_timer(Duration::from_millis((1000.0 * PHYSICS_DT) as u64))))
+        .add_systems(Update, (debug_points, debug_constraints))
         .run();
 }
 
-#[derive(Component)]
-pub struct Body {
-    pub shape: Shape,
+#[derive(Component, Reflect, InspectorOptions)]
+#[reflect(InspectorOptions)]
+pub struct Point {
     pub position: Vec2,
     pub velocity: Vec2,
-    pub origin: Vec2,
-    pub angular_velocity: f32,
-    pub air_friction: f32,
-    pub angular_friction: f32,
-    pub rotation: f32,
+    pub prev_position: Vec2,
+    pub radius: f32,
     pub mass: f32,
 }
 
-impl Default for Body {
-    fn default() -> Self {
-        Self {
-            shape: Shape::Circle { radius: 1.0 },
-            position: Vec2::ZERO,
-            velocity: Vec2::ZERO,
-            origin: Vec2::ZERO,
-            angular_velocity: 0.1,
-            air_friction: 0.92,
-            angular_friction: 0.9,
-            rotation: 0.0,
-            mass: 10.0,
-        }
-    }
+#[derive(Component, Reflect, InspectorOptions)]
+#[reflect(InspectorOptions)]
+pub struct Constraint {
+    pub point_a: Entity,
+    pub point_b: Entity,
+    pub length: f32,
+    pub strength: f32,
 }
 
-pub struct ForceTorque {
-    force: Vec2,
-    torque: f32,
-}
-
-impl Body {
-    /// kgm^2
-    pub fn moment_of_inertia(&self) -> f32 {
-        match &self.shape {
-            Shape::Circle { radius } => (self.mass * radius.powi(2)) / 2.0,
-            Shape::Rectangle { width, height } => {
-                (self.mass / 12.0) * (width.powi(2) + height.powi(2))
-            }
-        }
-    }
-
-    pub fn apply_displacement(&mut self, delta: ForceTorque) {
-        let vel = delta.force / self.mass;
-        self.velocity += vel;
-        self.angular_velocity += delta.torque / self.moment_of_inertia();
-    }
-
-    pub fn get_displacement(&self, from: Vec2, force: Vec2) -> ForceTorque {
-        let delta = from - self.position;
-        let distance = delta.length();
-        let torque = delta.perp_dot(force) * distance;
-        ForceTorque { force, torque }
-    }
-}
-
-pub enum Shape {
-    Circle { radius: f32 },
-    Rectangle { width: f32, height: f32 },
-}
-
-pub fn spawn_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
-}
-
-fn spawn_body(commands: &mut Commands, position: Vec2, shape: Shape) {
-    let transform = Transform::from_xyz(position.x, position.y, 0.0);
-    let shape_bundle = match &shape {
-        Shape::Circle { radius } => ShapeBundle {
-            path: GeometryBuilder::build_as(&Circle {
-                radius: *radius,
-                ..default()
-            }),
-            transform,
-            ..default()
-        },
-        Shape::Rectangle { width, height } => ShapeBundle {
-            path: GeometryBuilder::build_as(&Rectangle {
-                extents: Vec2 {
-                    x: *width,
-                    y: *height,
-                },
-                origin: RectangleOrigin::Center,
-            }),
-            transform,
-            ..default()
-        },
-    };
-    let mass = match &shape {
-        Shape::Circle { radius } => PI * radius.powi(2),
-        Shape::Rectangle { width, height } => width * height,
-    } / 100.0;
-    let mut body = Body {
-        shape,
-        mass,
-        velocity: position / -5.0,
+pub fn make_point(position: Vec2) -> impl Bundle {
+    Point {
         position,
-        ..default()
+        velocity: Vec2::ZERO,
+        prev_position: position,
+        radius: 1.0,
+        mass: 10.0,
+    }
+}
+
+pub fn make_constraint(point_a: Entity, point_b: Entity, length: f32) -> impl Bundle {
+    Constraint {
+        point_a,
+        point_b,
+        length,
+        strength: 1.0,
+    }
+}
+
+pub fn spawn_entities(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+    let a = commands.spawn(make_point(Vec2::new(0.0, 0.0))).id();
+    let b = commands.spawn(make_point(Vec2::new(100.0, 0.0))).id();
+    let c = commands.spawn(make_point(Vec2::new(100.0, 100.0))).id();
+    let d = commands.spawn(make_point(Vec2::new(0.0, 100.0))).id();
+    commands.spawn(make_constraint(a, b, 100.0));
+    commands.spawn(make_constraint(b, c, 100.0));
+    commands.spawn(make_constraint(c, d, 100.0));
+    commands.spawn(make_constraint(d, a, 100.0));
+}
+
+pub fn apply_velocities(mut point_query: Query<&mut Point>) {
+    for mut point in point_query.iter_mut() {
+        point.prev_position = point.position;
+        point.position = point.position + point.velocity;
+    }
+}
+
+pub fn debug_points(mut gizmos: Gizmos, point_query: Query<&Point>) {
+    for point in point_query.iter() {
+        gizmos.circle_2d(point.position, point.radius, Color::WHITE);
+    }
+}
+
+macro_rules! constraint_points {
+    ($constraint:ident, $point_query:ident, $point_a:ident, $point_b:ident, $then:block) => {
+        let point_a = $point_query.get($constraint.point_a);
+        if let Ok($point_a) = point_a {
+            let point_b = $point_query.get($constraint.point_b);
+            if let Ok($point_b) = point_b $then
+        }
     };
-    let ft = body.get_displacement(
-        body.position - Vec2 { x: 10.0, y: 0.0 },
-        Vec2 { x: 0.0, y: 10.0 },
-    );
-    match body.shape {
-        Shape::Circle { radius } => println!("new circle({})", radius),
-        Shape::Rectangle { width, height } => println!("new rectangle({}, {})", width, height),
-    }
-    println!("mass: {}", body.mass);
-    println!("inertia: {}", body.moment_of_inertia());
-    println!("{:?} {}", ft.force, ft.torque);
-    body.apply_displacement(ft);
-    commands.spawn((body, shape_bundle, Fill::color(Color::WHITE)));
 }
 
-pub fn spawn_bodies(mut commands: Commands) {
-    spawn_body(
-        &mut commands,
-        Vec2 { x: -100.0, y: 0.0 },
-        Shape::Rectangle {
-            width: 20.0,
-            height: 50.0,
-        },
-    );
-    spawn_body(
-        &mut commands,
-        Vec2 { x: 100.0, y: 0.0 },
-        Shape::Rectangle {
-            width: 100.0,
-            height: 10.0,
-        },
-    );
-}
-
-pub fn apply_velocities(mut body_query: Query<&mut Body>) {
-    for mut body in body_query.iter_mut() {
-        body.angular_velocity *= body.angular_friction.powf(PHYSICS_DT);
-        body.rotation += body.angular_velocity * PHYSICS_DT;
-
-        body.velocity = body.velocity * body.air_friction.powf(PHYSICS_DT);
-        body.position = body.position + body.velocity * PHYSICS_DT;
-    }
-}
-
-pub fn update_transforms(mut body_query: Query<(&Body, &mut Transform)>) {
-    for (body, mut transform) in body_query.iter_mut() {
-        transform.translation.x = body.position.x;
-        transform.translation.y = body.position.y;
-        transform.rotation = Quat::from_rotation_z(body.rotation);
+pub fn debug_constraints(
+    mut gizmos: Gizmos,
+    point_query: Query<&Point>,
+    constraint_query: Query<&Constraint>,
+) {
+    for constraint in constraint_query.iter() {
+        constraint_points!(constraint, point_query, point_a, point_b, {
+            gizmos.line_2d(point_a.position, point_b.position, Color::RED);
+        });
     }
 }
